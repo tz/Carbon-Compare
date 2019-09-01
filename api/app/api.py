c3e84127ffa6404ac5bc5571b2607fd6375b7c0f
Ted Zellers <ted@tedzellers.com>
2019-09-01 02:40:53 -0400

    API: add CORS, joins

--- api/app/api.py
+++ api/app/api.py
@@ -1,6 +1,7 @@
 from bson import ObjectId
 import os
 from pymongo import MongoClient
+import json
 
 class DB:
     def __init__(self):
@@ -16,13 +17,29 @@ class DB:
         else:
             raise Exception('bad query limit')
 
+    def aggregate(self, collection, query):
+        return self._db[collection].aggregate(query)
+
     def __del__(self):
         self._client.close()
-        
+
 def get_all_subjects():
+    return '[{"name": "Beef", "img": "cow.png", "impact_units": "3.00", "category": "5d2678721c9d440000da14ae"}, {"name": "Legumes", "img": "pig.png", "impact_units": ".05", "category": "5d2678721c9d440000da14ae"}, {"name": "Pork", "img": "beans.png", "impact_units": ".781", "category": "5d2678721c9d440000da14ae"}, {"name": "Poultry", "img": "chicken.png", "impact_units": ".572", "category": "5d2678721c9d440000da14ae"}, {"name": "Potatoes", "img": "potatoes.png", "impact_units": ".01", "category": "5d2678721c9d440000da14ae"}, {"name": "Average Car", "img": "plane.png", "impact_units": ".35", "category": "5d267b8c36a05e4aaba3a9de"}]'
     db = DB()
-    return '{}'.format([s for s in db.query('Subjects', {})])
+    subjects = [s for s in db.aggregate("Subjects", [{'$lookup':{'from': 'Images','localField': 'image','foreignField': '_id','as': 'img'}},{'$project':{'name':1,'img.file':1,'impact_units':1,'category':1}}])]
+    subjects = [{'name': s['name'], 'img': s['img'][0]['file'], 'impact_units': s['impact_units'], 'category': str(s['category'])} for s in subjects]
+    print('{}'.format(json.dumps(subjects)))
+    return '{}'.format(json.dumps(subjects))
+
+def get_categories():
+    return '[{"name": "food", "units": "serving", "img": "burger.png", "id": "5d2678721c9d440000da14ae"}, {"name": "transportation", "units": "passenger kilometer", "img": "plane.png", "id": "5d267b8c36a05e4aaba3a9de"}]'
+    db = DB()
+    cats = [c for c in db.aggregate("Types", [{'$lookup':{'from': 'Images','localField': 'image','foreignField': '_id','as': 'img'}},{'$project':{'name':1,'units':1, 'img.file':1, '_id':1}}])]
+    cats = [{'name': c['name'], 'units': c['units'], 'img': c['img'][0]['file'], 'id': str(c['_id'])} for c in cats]
+    # print('{}'.format(json.dumps(cats)))
+    return '{}'.format(json.dumps(cats))
+
 
 def get_subject(id):
     db = DB()
-    return '{}'.format(db.query('Subjects', {"_id": ObjectId(id)}, 1))
+    return '{}'.format(json.dumps(db.query('Subjects', {"_id": ObjectId(id)}, 1)))
--- api/routes.py
+++ api/routes.py
@@ -1,16 +1,22 @@
 from app import api
 
 from flask import Flask, url_for, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return 'ok'
 
 @app.route('/api/subjects')
-def get_data():
+def get_subjects():
     return api.get_all_subjects()
+
+@app.route('/api/categories')
+def get_categories():
+    return api.get_categories()
 
 @app.route('/api/subject/<string:subject_id>')
 def get_subject(subject_id):
